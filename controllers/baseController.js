10402d23f9754d509b185adf70de2154e2adf851
cosmo4 <warner.luke0102@gmail.com>
2024-05-13 16:38:31 -0600

    removed old function

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -30,22 +30,6 @@ baseController.getOneContact = async function(req, res) {
     }
 }
 
-baseController.createUser = async function(req, res) {
-    try {
-        let result = await mongodb.getDb().db().collection('people').insertOne({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            favoriteColor: req.body.favoriteColor,
-            birthday: req.body.birthday
-        })
-
-    } catch (error) {
-        console.error('Error adding user:', error);
-        res.status(500).json({ message: 'Internal server error' })
-    }
-}
-
 baseController.createContact = async function(req, res) {
     try {
         const newContact = req.body;
